fae02222959ae3bb6753e294a7e4a791a83b9cc9
Sergey Lisakonov <lisakonov@Ukr.net>
2020-03-04 14:19:09 +0200

    Add new

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const DEFAULT_COMMAND = `help`;
+const USER_ARGV_INDEX = 2;
+const EXIT_CODE = {
+  error: 1,
+  success: 0,
+};
+
+module.exports = {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  EXIT_CODE
+};
